34104f95c83f9b5738ec502e27a938d19a46ee24
Sofia Zaozerskaya <sgfx@Sofis-MacBook-Air-9.local>
2025-02-15 17:55:35 -0800

    landing page and buttons

--- Screens/Landing.js
+++ Screens/Landing.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+
+import * as Colors from '../components/Colors'
+import { PurpleButton, WhiteButton } from '../components/Button';
 import { StyleSheet, Text, View, Button, Platform } from 'react-native';
 
 export default function Landing({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={{fontFamily: "Inconsolata-Regular"}}>
-      Landing Page</Text>
-      <Button title="Login" onPress={() => navigation.navigate('BottomTab', { uid: '123' })} />
-      <Button title="Signup" onPress={() => navigation.navigate('Signup')} />
+      <Text style={styles.title} marginTop={'45%'} marginBottom={'10%'}>hey there</Text>
+      <Text style={styles.subtitle} marginBottom={'0%'}>this is [perspective],</Text>
+      <Text style={styles.subtitle} marginBottom={'30%'}>your cognition copilot</Text>
+
+      <PurpleButton title="login" marginBottom={'7%'} onPress={() => navigation.navigate('login', { uid: '123' })} />
+      <WhiteButton title="sign up" marginBottom={'10%'} onPress={() => navigation.navigate('signup')} />
     </View>
   );
 }
@@ -19,5 +24,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily: "Inconsolata-Regular",
   },
+  title: {
+    fontSize: 40,
+    // bold text
+    fontWeight: 'bold',
+    color: Colors.primary,
+    fontFamily: "Inconsolata-Regular",
+  },
+  subtitle: {
+    fontSize: 25,
+    fontWeight: 600,
+    color: Colors.secondary,
+    marginTop: 10,
+    fontFamily: "Inconsolata-Regular",
+  }
 });
